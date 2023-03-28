b44ce40bafdc8aeee0227583f01bb6545c8bad53
Alex <ivlevaleks94@mail.ru>
2023-03-28 22:02:27 +0700

    added changes

--- HW_1/task_3.java
+++ HW_1/task_3.java
@@ -5,13 +5,72 @@ import java.util.Scanner;
 public class task_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int num = sc.nextInt();
-        System.out.print("Введите число: ");
-        int num_2 = sc.nextInt();
+        double num_1 = sc.nextDouble();
+        double num_2 = sc.nextDouble();
+        double result;
         System.out.print("Введите знак: ");
-        String symbol = sc.next();
-        
-        
+        String sign = sc.next();
+
+        if (sign.equals("+")) {
+            result = num_1 + num_2;
+            System.out.println(result);
+        } else if (sign.equals("-")) {
+            result = num_1 - num_2;
+            System.out.println(result);
+        } else if (sign.equals("*")) {
+            result = num_1 * num_2;
+            System.out.println(result);
+        } else if (sign.equals("/")) {
+            if (num_2 != 0) {
+                result = num_1 / num_2;
+                System.out.println(result);
+            } else {
+                System.out.println("На ноль делить нельзя");
+            }
+        } else {
+            System.out.println("Введен некорректный знак, введите снова");
+            sc.close();
+        }
     }
 }
+
+// public class task_3 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         double num_1 = sc.nextDouble();
+//         double num_2 = sc.nextDouble();
+//         double result;
+//         System.out.print("Введите знак: ");
+//         String sign = sc.next();
+
+//         switch (sign) {
+//             case "+": {
+//                 result = num_1 + num_2;
+//                 System.out.println(result);
+//                 break;
+//             }
+//             case "-": {
+//                 result = num_1 - num_2;
+//                 System.out.println(result);
+//                 break;
+//             }
+//             case "*": {
+//                 result = num_1 * num_2;
+//                 System.out.println(result);
+//                 break;
+//             }
+//             case "/": {
+//                 if (num_2 != 0) {
+//                     result = num_1 / num_2;
+//                     System.out.println(result);
+//                     break;
+//                 } else {
+//                     System.out.println("На ноль делить нельзя");
+//                 }
+//             }
+//             default:
+//                 System.out.println("Введен некорректный знак, введите снова");
+//                 sc.close();
+//         }
+//     }
+// }
