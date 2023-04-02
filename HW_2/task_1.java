de004f90b1bd697658911212e1b53472e05e3155
Alex <ivlevaleks94@mail.ru>
2023-04-02 23:37:30 +0700

    added info task_2

--- HW_2/task_1.java
+++ HW_2/task_1.java
@@ -1,5 +1,7 @@
 package H_W.HW_2;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 // Реализуйте алгоритм сортировки пузырьком числового массива(введен вами), 
@@ -7,16 +9,34 @@ import java.util.Arrays;
 
 public class task_1 {
     public static void main(String[] args) {
-        int[] array = new int [] {5, 7, 2, 8, 3, 4, 6};
-        // System.out.println(Arrays.toString(array));
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+        String sort_file = "file.txt";
+        File file = new File(sort_file);
+       
+        try {
+            FileWriter writer_sort = new FileWriter(file, false);
+            int[] array = new int [] {5, 7, 2, 8, 3, 4, 6};
+
+            System.out.println(Arrays.toString(array) + "\n");
+            writer_sort.write(Arrays.toString(array) + "\n");
+            writer_sort.write("\n");
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
                 }
-            } System.out.println(Arrays.toString(array));
-        } 
+                writer_sort.write(Arrays.toString(array));
+                writer_sort.write("\n");
+                System.out.println(Arrays.toString(array));
+            } 
+            writer_sort.close();
+            System.out.println("Получилось!");
+
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так");
+        }     
     }
 }
